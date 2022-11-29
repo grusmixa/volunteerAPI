40b6ff30148fcc3d68c72eecacdb6e60ec96dcee
Ildar <ildarthegreat@gmail.com>
2022-11-29 23:44:34 +0400

    init: init commit

---
+++ src/main/java/ru/ssau/volunteerapi/VolunteerApiApplication.java
@@ -0,0 +1,13 @@
+package ru.ssau.volunteerapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VolunteerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VolunteerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/ssau/volunteerapi/VolunteerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ssau.volunteerapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VolunteerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
